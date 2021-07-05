6c40a60e396216a45f0d93c50f8750893251017a
unknown <Ginger@NEMAC-WLP5008.uncanet.unca.edu>
2021-07-05 16:30:12 -0400

    update revisions to Guam indices

--- LBII_clip.R
+++ LBII_clip.R
@@ -5,10 +5,12 @@ library(sf)
 GL_boundary <- "D:/NFWF_PhaseIII/Preliminary_Boundaries/GL_PrelimBndy.shp"
 AS_boundary <- "D:/NFWF_PhaseIII/Preliminary_Boundaries/AS_PrelimBoundary.shp"
 GU_boundary <- "D:/NFWF_PhaseIII/Preliminary_Boundaries/Guam_PrelimBndy.shp"
-AK_boundary <- "D:/Packages/AK_Draft_Indices_012721_6145f6/commondata/boundaries/AK_20mDepth_Boundary_v1"
+AK_boundary <- "D:/Packages/AK_Draft_Indices_012721_6145f6/commondata/boundaries/AK_20mDepth_Boundary_v1.shp"
 
-thepath <- "D:/Datasets/Local_Biodiversity_Intactness/lbii.asc"
-shppath <- AS_boundary  # GU boundary, AK boundary
+lbii_path <- "D:/Datasets/Local_Biodiversity_Intactness/second_download_6-15-21/lbii.asc"
+# fli_path <- "D:/Datasets/Forest_Landscape_Integrity/forest_landscape_integrity_earth.tif"
+thepath <- lbii_path
+shppath <- GU_boundary  # AS_boundary  # GU boundary, AK boundary
 lbii_ras <- raster(thepath)
 
 # set projection of LBII (assume WGS84 lat long)
@@ -21,5 +23,5 @@ pr_wgs84 <- st_transform(pr_boundary, crs=CRS('+init=EPSG:4326'))
 # crop LBII to NFWF boundary
 lbii_pr <- crop(x=lbii_ras, y=pr_wgs84)
 # out_path <- "D:/Datasets/Local_Biodiversity_Intactness/lbii_Great_Lakes.tif"  # GU, AS, AK
-out_path <- "D:/Datasets/Local_Biodiversity_Intactness/lbii_AS.tif"  # GU, AS, AK
+out_path <- "D:/Datasets/Local_Biodiversity_Intactness/lbii_AK.tif"  # GU, AS, AK
 writeRaster(lbii_pr, out_path, format='GTiff')
--- Revise_indices.py
+++ Revise_indices.py
@@ -74,8 +74,8 @@ def raster_list_sum(
 
 
 def revise_index_with_additions(
-        new_input_dict, intermediate_dir, boundary_path, existing_index_path,
-        revised_index_path):
+        new_input_dict, intermediate_dir, boundary_path, template_path,
+        revised_index_path, existing_index_path=None):
     """Revise an existing index by adding vector-based features.
 
     All new inputs must share spatial reference with the existing index.
@@ -92,10 +92,14 @@ def revise_index_with_additions(
             should be written
         boundary_path (string): path to shapefile giving the boundary of the
             study area, to which the revised index should be clipped
-        existing_index_path (string): path to existing index to which new
-            additions should be made
+        template_path (string): path to raster that should be used as a
+            template. The projection, extent, and resolution of the revised
+            raster will match this template
         revised_index_path (string): path to location where revised index
             should be saved
+        existing_index_path (string, optional): path to existing index to which
+            new additions should be made. If not given, the revised index is
+            calculated as the sum of new additions
 
     Side effects:
         creates a raster in `intermediate_dir` for each new input
@@ -107,7 +111,7 @@ def revise_index_with_additions(
     """
     if not os.path.exists(intermediate_dir):
         os.makedirs(intermediate_dir)
-    raster_info = pygeoprocessing.get_raster_info(existing_index_path)
+    raster_info = pygeoprocessing.get_raster_info(template_path)
     input_datatype = raster_info['datatype']
     input_nodata = raster_info['nodata'][0]
     pixel_size = raster_info['pixel_size']
@@ -134,7 +138,7 @@ def revise_index_with_additions(
         intermediate_path_dict[new_input] = os.path.join(
             intermediate_dir, '{}.tif'.format(new_input))
         pygeoprocessing.new_raster_from_base(
-            existing_index_path, intermediate_path_dict[new_input],
+            template_path, intermediate_path_dict[new_input],
             input_datatype, [input_nodata], fill_value_list=[input_nodata])
         pygeoprocessing.rasterize(
             new_input_dict[new_input]['path'],
@@ -146,8 +150,12 @@ def revise_index_with_additions(
             prefix='revised_index_not_clipped', suffix='.tif',
             delete=False) as unclipped_raster_file:
         unclipped_raster_path = unclipped_raster_file.name
-    sum_path_list = [existing_index_path] + [
-        intermediate_path_dict[i] for i in intermediate_path_dict]
+    if existing_index_path:
+        sum_path_list = [existing_index_path] + [
+            intermediate_path_dict[i] for i in intermediate_path_dict]
+    else:
+        sum_path_list = [
+            intermediate_path_dict[i] for i in intermediate_path_dict]
     raster_list_sum(
         sum_path_list, input_nodata, unclipped_raster_path, input_nodata,
         input_datatype, nodata_remove=True)
@@ -175,7 +183,7 @@ def AS_threat_index_v2():
     revised_index_path = "D:/Packages/AS_Threat_Inputs_v1_01252021_de5873/commondata/raster_data10/AS_Threat_Index_v2.tif"
     revise_index_with_additions(
         new_input_dict, intermediate_dir, boundary_path, existing_index_path,
-        revised_index_path)
+        revised_index_path, existing_index_path)
 
 
 def AS_terrestrial_index_revisions():
@@ -200,7 +208,7 @@ def AS_terrestrial_index_revisions():
     revised_index_path = "D:/Packages/AS_Wildlife_Inputs_v1_01252021_52abd4/commondata/raster_data6/AS_Terrestrial_Index_ALL_v2.tif"
     revise_index_with_additions(
         new_input_dict, intermediate_dir, boundary_path, existing_index_path,
-        revised_index_path)
+        revised_index_path, existing_index_path)
 
 
 def GU_marine_index_revisions():
@@ -217,7 +225,7 @@ def GU_marine_index_revisions():
     revised_index_path = TODO
     revise_index_with_additions(
         new_input_dict, intermediate_dir, boundary_path, existing_index_path,
-        revised_index_path)
+        revised_index_path, existing_index_path)
 
 
 def GU_terrestrial_index_revisions():
@@ -242,7 +250,7 @@ def GU_terrestrial_index_revisions():
     revised_index_path = "D:/Packages/GU_Wildlife_Index_v1revised1_e193be/GU_Terrestrial_Index_ALL_v2.tif"
     revise_index_with_additions(
         new_input_dict, intermediate_dir, boundary_path, existing_index_path,
-        revised_index_path)
+        revised_index_path, existing_index_path)
 
 
 def GU_threat_index_revisions():
@@ -275,7 +283,7 @@ def GU_threat_index_revisions():
     intermediate_index_path = "D:/Packages/GU_Threat_Inputs_v1_01252021_71a7fb/commondata/raster_data10/GU_Threat_Index_v2_intermediate.tif"
     revise_index_with_additions(
         new_input_dict, intermediate_dir, boundary_path, existing_index_path,
-        intermediate_index_path)
+        intermediate_index_path, existing_index_path)
 
     # add wave exposure, reclassified in Arc to 1-5
     wave_exp_path = "D:/NFWF_PhaseIII/Guam/Data - digital atlas of Southern Guam/coast-wave-exposure/wave_exposure_5class_proj2.tif"
@@ -287,7 +295,6 @@ def GU_threat_index_revisions():
             prefix='revised_index_not_clipped', suffix='.tif',
             delete=False) as unclipped_raster_file:
         unclipped_raster_path = unclipped_raster_file.name
-        sum_path_list = [intermediate_index_path, wave_exp_path]
     
     sum_path_list = [intermediate_index_path, wave_exp_path]
     raster_list_sum(
@@ -305,10 +312,120 @@ def GU_threat_index_revisions():
     os.remove(unclipped_raster_path)
 
 
+def GU_asset_index_revisions():
+    """Revise asset index with new data following workshop."""
+    # calculate critical infrastructure input: all footprints get rank=5
+    c_infrastructure_input_dict = {
+        'marinas': {
+            'path': "D:/Packages/GU_Asset_Inputs_v1_01252021_4fc023/commondata/footprints_v1/GU_Marinas_v1.shp",
+            'rank': 5,
+        },
+        'communication': {
+            'path': "D:/Packages/GU_Asset_Inputs_v1_01252021_4fc023/commondata/footprints_v1/GU_Communication_Buffer_v1.shp",
+            'rank': 5,
+        },
+        'petro': {
+            'path': "D:/Packages/GU_Asset_Inputs_v1_01252021_4fc023/commondata/footprints_v1/GU_Petro_Terminals_Footprints_v1.shp",
+            'rank': 5,
+        },
+        'ports': {
+            'path': "D:/Packages/GU_Asset_Inputs_v1_01252021_4fc023/commondata/footprints_v1/GU_Ports_Footprints_v1.shp",
+            'rank': 5,
+        },
+        'roads': {
+            'path': "D:/Packages/GU_Asset_Inputs_v1_01252021_4fc023/commondata/footprints_v1/GU_Primary_Roads_Buffer_v1.shp",
+            'rank': 5,
+        },
+        'airports': {
+            'path': "D:/Packages/GU_Asset_Inputs_v1_01252021_4fc023/commondata/footprints_v1/GU_Airport_Runways_Buffer_v1.shp",
+            'rank': 5,
+        },
+        'dams': {
+            'path': "D:/Packages/GU_Asset_Inputs_v1_01252021_4fc023/commondata/footprints_v1/GU_Dam_Lines_Buffer_v1.shp",
+            'rank': 5,
+        },
+        'wwtp': {
+            'path': "D:/Packages/GU_Asset_Inputs_v1_01252021_4fc023/commondata/footprints_v1/GU_WWTP_Footprints_v1.shp",
+            'rank': 5,
+        },
+        'hazfacs': {
+            'path': "D:/Packages/GU_Asset_Inputs_v1_01252021_4fc023/commondata/footprints_v1/GU_HazFacs_Footprints_v1.shp",
+            'rank': 5,
+        },
+        'electricity': {
+            'path': "D:/Packages/GU_Asset_Inputs_v1_01252021_4fc023/commondata/footprints_v1/GU_Electricity_Footprints_v1.shp",
+            'rank': 5,
+        },
+        # TODO cultural/historic sites????
+    }
+    intermediate_dir = "D:/NFWF_PhaseIII/Guam/Data - Exposure Index/asset_index_revisions"
+    boundary_path = "D:/Packages/GU_Wildlife_Inputs_v1_01252021_c4e628/commondata/boundaries/GU_30m_Boundary.shp"
+    template_path = "D:/Packages/GU_Asset_Inputs_v1_01252021_4fc023/commondata/raster_data6/GU_Exposure_Index_10class_v1.tif"
+    crit_infrastructure_path = "D:/Packages/GU_Asset_Inputs_v1_01252021_4fc023/commondata/raster_data1/GU_C_Infrastructure_v2.tif"
+    revise_index_with_additions(
+        c_infrastructure_input_dict, intermediate_dir, boundary_path,
+        template_path, revised_index_path)
+
+    # revise critical facilities input: additional features
+    c_facilities_input_dict = {
+        'hospitals_add': {
+            'path': "D:/Packages/GU_Asset_Inputs_v1_01252021_4fc023/commondata/footprints_v1/hospitals_add.shp",
+            'rank': 5,
+        },
+        'private_schools_add': {
+            'path': "D:/Packages/GU_Asset_Inputs_v1_01252021_4fc023/commondata/footprints_v1/private_schools_add.shp",
+            'rank': 5,
+        },
+        'private_schools_add2': {
+            'path': "D:/Packages/GU_Asset_Inputs_v1_01252021_4fc023/commondata/footprints_v1/private_schools_add2.shp",
+            'rank': 5,
+        },
+        'urgentcare_hospitals_add': {
+            'path': "D:/Packages/GU_Asset_Inputs_v1_01252021_4fc023/commondata/footprints_v1/urgentcare_hospitals_add.shp",
+            'rank': 5,
+        },
+        'urgentcare_hospitals_add2': {
+            'path': "D:/Packages/GU_Asset_Inputs_v1_01252021_4fc023/commondata/footprints_v1/urgentcare_hospitals_add2.shp",
+            'rank': 5,
+        },
+    }
+    existing_index_path = "D:/Packages/GU_Asset_Inputs_v1_01252021_4fc023/commondata/raster_data2/GU_C_Facilities_v1.tif"
+    crit_facilities_path = "D:/Packages/GU_Asset_Inputs_v1_01252021_4fc023/commondata/raster_data2/GU_C_Facilities_v2.tif"
+    revise_index_with_additions(
+        c_facilities_input_dict, intermediate_dir, boundary_path,
+        template_path, crit_facilities_path, existing_index_path)
+
+    # recalculate asset index
+    raster_info = pygeoprocessing.get_raster_info(template_path)
+    input_datatype = raster_info['datatype']
+    input_nodata = raster_info['nodata'][0]
+    pixel_size = raster_info['pixel_size']
+    with tempfile.NamedTemporaryFile(
+            prefix='revised_asset_index_not_clipped', suffix='.tif',
+            delete=False) as unclipped_raster_file:
+        unclipped_raster_path = unclipped_raster_file.name
+    
+    sum_path_list = [crit_infrastructure_path, crit_facilities_path]
+    raster_list_sum(
+        sum_path_list, input_nodata, unclipped_raster_path, input_nodata,
+        input_datatype, nodata_remove=True)
+
+    # clip their sum to the region
+    asset_index_path = "D:/Packages/GU_Asset_Inputs_v1_01252021_4fc023/commondata/raster_data5/GU_Asset_Index_ALL_v2.tif"
+    pygeoprocessing.align_and_resize_raster_stack(
+        [unclipped_raster_path], [asset_index_path], ['near'],
+        pixel_size, 'intersection', base_vector_path_list=[boundary_path],
+        raster_align_index=0)
+    
+    # clean up
+    os.remove(unclipped_raster_path)
+
+
 if __name__ == "__main__":
     # AS_threat_index_v2()
     # AS_terrestrial_index_revisions()
     # GU_marine_index_revisions()
     # GU_terrestrial_index_revisions()
     # GU_threat_index_revisions()
+    # GU_asset_index_revisions()
 
